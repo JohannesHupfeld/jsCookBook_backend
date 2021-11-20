53e57eb3a078d028e3659475017580941c71e220
JohannesHupfeld <j.hupfeld@gmail.com>
2021-11-20 14:58:14 -0500

    created index and create methods

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::RecipesController < ApplicationController
+
+  def index
+    recipes = Recipe.all
+      # no need for insance variables since we are not rendering to an erb but redirecting with fetch requests
+    render json: recipes
+  end
+
+  def create
+    recipe = Recipe.new(recipe_params)
+    if recipe.save
+      render json: recipe, status: :accepted 
+    else
+      render json: {errors: recipe.errors.full_messages}, status: :unprocessable_entity
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :ingredients, :instructions, :image_url, :category_id)
+  end
+
 end
