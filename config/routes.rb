f43568b9dbf4e0510d1caefcc50a6de40f527779
JohannesHupfeld <j.hupfeld@gmail.com>
2021-11-20 16:37:09 -0500

    created categories controller and added category route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do # since our front end application might be hosted on a specific domain, we will want all of our backend routes to be name spaced to indicate they are routes associated with an api.
     namespace :v1 do
       resources :recipes, only: [:index, :create]
+      resources :categories, only: [:index]
     end
   end
 end
