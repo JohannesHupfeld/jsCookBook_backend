20168e973fb995c8be0f05caf81910118c0728a7
JohannesHupfeld <j.hupfeld@gmail.com>
2021-11-20 21:07:37 -0500

    reverted back to namespaced convention with version folders in controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :recipes, only: [:index, :create]
-  resources :categories, only: [:index]
+
+  # since our front end application might be hosted on a specific domain, we will want all of our backend routes to be name spaced to indicate they are routes associated with an api.
+  namespace :api do
+    namespace :v1 do
+      resources :recipes, only: [:index, :create]
+      resources :categories, only: [:index]
+    end
+  end
 end
